11f4c538678323e045f3c2f0191719d939fcfa67
Jack Nehme <jack.nehme@gmail.com>
2017-10-09 22:38:00 -0700

    Added webpack features wip

---
+++ webpack.config.prod.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+
+export default {
+    debug: true,
+    devtool: 'source-map',
+    noInfo: false,
+    entry: {
+        vendor: path.resolve(__dirname, 'src/vendor'), // 'vendor' name used on line 23
+        main: path.resolve(__dirname, 'src/index')
+    },
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].[chunkhash].js'
+    },
+    plugins: [
+        // Hash the files using MD5 so that their names change when the content changes.
+        new WebpackMd5Hash(),
+
+        // Use CommonsChunkPlugin to create a separate bundle
+        // of vendor libraries so that they're cached separately. /* Chunk == bundle */
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+
+        // Create HTML file that includes reference to bundled JS.
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            },
+            inject: true
+        }),
+        // Elimainate duplicate packages when generating bundle
+        new webpack.optimize.DedupePlugin(),
+        // Minify JS
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+            { test: /\.css$/, loaders: ['style', 'css'] }
+        ]
+    }
+}
