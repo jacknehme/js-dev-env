988b22ae9fc07b0cee75058cfbe4580a62128867
Jack Nehme <jack.nehme@gmail.com>
2017-10-16 21:49:22 -0700

    Deployed app using Heroku and Surge.sh

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -16,15 +16,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res) {
-    // harding coding for simplicity. Prtend this hits a real database
-    res.json([
-        { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
-        { "id": 1, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@gmail.com" },
-        { "id": 1, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@gmail.com" },
-    ]);
-});
-
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3002/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3002/' : 'https://morning-fjord-59539.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
